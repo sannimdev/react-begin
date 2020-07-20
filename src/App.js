5c4d9fb375d61905922b1f9e1a7d8419af07d89a
Sannim <inegg.apps@gmail.com>
2020-07-20 19:07:31 +0900

    useState, onClick을 이용하여 상태 관리하기

--- src/App.js
+++ src/App.js
@@ -2,15 +2,12 @@ import React from "react";
 import Hello from "./Hello";
 import "./App.css";
 import Wrapper from "./Wrapper";
+import Counter from "./Counter";
 
 function App() {
   return (
     <>
-      <Wrapper>
-        {/* 속성 값을 지정하지 않으면 기본으로 true로 지정한다 */}
-        <Hello name="react" color="red" isSpecial />
-        <Hello isSpecial={false} />
-      </Wrapper>
+      <Counter />
     </>
   );
 }
