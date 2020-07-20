10f9fae5b5597d307f63a8c43d3732634ee81b89
Sannim <inegg.apps@gmail.com>
2020-07-20 19:15:48 +0900

    Input 상태 관리하기 예제

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React from "react";
 import Hello from "./Hello";
 import "./App.css";
-import Wrapper from "./Wrapper";
-import Counter from "./Counter";
+import InputSample from "./InputSample";
 
 function App() {
   return (
     <>
-      <Counter />
+      <InputSample />
     </>
   );
 }
