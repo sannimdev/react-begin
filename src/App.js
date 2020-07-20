ff8f4a12e97247a6a2b91eea5cdc04d164feedcb
Sannim <inegg.apps@gmail.com>
2020-07-20 17:45:49 +0900

    React에서 1. style 속성을 지정하는 방법 2. className을 지정하는 방법 3. 주석을 사용하는 방법 4. css에 저장한 속성을 불러오는 방법

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
 import React from "react";
 import Hello from "./Hello";
+import "./App.css";
 
 function App() {
+  const name = "react";
+  const style = {
+    backgroundColor: "red",
+    fontSize: 24,
+    color: "white",
+    padding: "1rem",
+  };
   return (
-    <div>
-      <Hello />
-      <Hello />
-      <Hello />
-    </div>
+    <>
+      {/*ddd */}
+      <Hello
+      //우와 신기하다
+      />
+      <div
+        //우와 신기해
+        style={style}
+      >
+        {name}
+      </div>
+      <div className="gray-box"></div>
+    </>
   );
 }
 
