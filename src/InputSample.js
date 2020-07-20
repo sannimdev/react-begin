d96901c0e6e13be29e73622e0896dd8f102a4098
Sannim <inegg.apps@gmail.com>
2020-07-20 19:27:57 +0900

    여러 개의 Input상자에 관한 다중 상태 관리하기 (useState를 객체로 관리하는 경우)

--- src/InputSample.js
+++ src/InputSample.js
@@ -1,22 +1,35 @@
 import React, { useState } from "react";
 
 function InputSample() {
-  const [text, setText] = useState("");
-
+  const [inputs, setInputs] = useState({
+    name: "",
+    nickname: "",
+  });
+  const { name, nickname } = inputs;
   const onChange = (e) => {
-    setText(e.target.value);
+    const { name, value } = e.target;
+    const nextInputs = {
+      //기존의 상태를 복사한 뒤 변경된 상태만 덮어씌워야 한다.
+      //이는 리액트에서 "불변성을 지킨다"고 한다.
+      //이에 따라 리액트는 변경사항을 감지하고 필요한 부분만 렌더링을 하게 된다.
+      ...inputs,
+      [name]: value, //[name]의 키항목이 name이면 이름을 변경, nickname이면 nickname을 변경하도록 유동적으로 지정이 가능
+    };
+    // nextInputs[name] = value;
+    setInputs(nextInputs);
   };
 
   const onReset = () => {
-    setText("");
+    setInputs({ name: "", nickname: "" });
   };
   return (
     <div>
-      <input onChange={onChange} value={text} />
+      <input name="name" placeholder="이름" onChange={onChange} value={name} />
+      <input name="nickname" placeholder="닉네임" onChange={onChange} value={nickname} />
       <button onClick={onReset}>초기화</button>
       <div>
         <strong>값 </strong>
-        {text}
+        {name} ({nickname})
       </div>
     </div>
   );
