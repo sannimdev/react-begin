5b6b3bfaac6bad7cbd53b358dc1cba1d0d8773af
Sannim <inegg.apps@gmail.com>
2020-07-21 16:40:55 +0900

    CreateUser컴포넌트를 이용하여 user목록 추가하기 (with useState) 이벤트리스너를 추가하여 컴포넌트에서 발생한 이벤트를 App.js에서 처리하기

--- src/App.js
+++ src/App.js
@@ -1,12 +1,73 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import Hello from "./Hello";
 import "./App.css";
 import UserList from "./UserList";
+import CreateUser from "./CreateUser";
 
 function App() {
+  const [inputs, setInputs] = useState({
+    username: "",
+    email: "",
+  });
+  const { username, email } = inputs;
+  const onChange = (e) => {
+    const { name, value } = e.target;
+    setInputs({
+      ...inputs,
+      [name]: value,
+    });
+  };
+  const [users, setUsers] = useState([
+    {
+      id: 1,
+      username: "velopert",
+      email: "public.velopert@gmail.com",
+      active: true,
+    },
+    {
+      id: 2,
+      username: "tester",
+      email: "tester@example.com",
+      active: false,
+    },
+    {
+      id: 3,
+      username: "liz",
+      email: "liz@example.com",
+      active: false,
+    },
+  ]);
+  //nextId값이 바뀐다고 해서 굳이 렌더링될 필요가 없다.
+  //바로 이렇게 렌더링에 관련되지 않는 값들을 관리하고자 할 때도 useRef를 사용하기도 한다.
+  //어떠한 변수를 기억하는데, 렌더링될 필요가 없는 사항들도 useRef로 관리하면 된다.
+  const nextId = useRef(4);
+
+  const onCreate = () => {
+    const user = {
+      id: nextId.current,
+      username,
+      email,
+    };
+    setUsers([...users, user]);
+    setInputs({
+      username: "",
+      email: "",
+    });
+    nextId.current += 1;
+  };
+
+  const onRemove = (id) => {
+    setUsers(users.filter((user) => user.id != id));
+  };
+
+  const onToggle = (id) => {
+    setUsers(users.map((user) => (user.id === id ? { ...user, active: !user.active } : user)));
+  };
+
   return (
     <>
-      <UserList />
+      <CreateUser username={username} email={email} onChange={onChange} onCreate={onCreate} />
+      <UserList users={users} onRemove={onRemove} onToggle={onToggle} />
     </>
   );
 }
--- src/UserList.js
+++ src/UserList.js
@@ -1,32 +1,26 @@
 import React from "react";
 
-function User({ user }) {
+function User({ user, onRemove, onToggle }) {
+  const { username, email, id, active } = user;
   return (
     <div>
-      <b>{user.username}</b>
-      <span>({user.email})</span>
+      <b
+        onClick={() => onToggle(id)}
+        style={{
+          color: active ? "green" : "black",
+          cursor: "pointer",
+        }}
+      >
+        {username}
+      </b>
+      &nbsp;
+      <span>({email})</span>
+      <button onClick={() => onRemove(id)}>삭제</button>
     </div>
   );
 }
 
-function UserList() {
-  const users = [
-    {
-      id: 1,
-      username: "velopert",
-      email: "public.velopert@gmail.com",
-    },
-    {
-      id: 2,
-      username: "tester",
-      email: "tester@example.com",
-    },
-    {
-      id: 3,
-      username: "liz",
-      email: "liz@example.com",
-    },
-  ];
+function UserList({ users, onRemove, onToggle }) {
   return (
     <div>
       {users.map((user, idx) => (
@@ -35,7 +29,7 @@ function UserList() {
         //왜냐하면, 배열의 개수나 원소의 값이 바뀔 때마다 어떠한 것을 렌더링하는지 리액트가 1:1 대응하는 정보를 알지 못하기 때문에...
         //렌더링 성능에서 비효율적으로 동작하게 될 가능성이 높기 때문이다.
         //단순히 새로운 원소가 중간에 삽입되거나 삭제된 경우에도 배열의 내용을 모두 덮어씌워버리고 남는 것이 있거나 모자라면 버리거나 추가하게 된다.
-        <User user={user} key={user.id} />
+        <User user={user} key={user.id} onRemove={onRemove} onToggle={onToggle} />
         //그러나 고윳값이 있는 객체들이 존재한다면?
         //렌더링하기 전에 이미 리액트는 배열의 원소에서 어떠한 값을 렌더링하는지 1:1로 알고 있기 때문에
         //원소의 변화를 감지하고 효율적으로 렌더링 작업을 수행할 수 있다.
