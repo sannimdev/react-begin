2c83f1c0702e2448be7c8b2b939f7d6cf3f50c5d
Sannim <inegg.apps@gmail.com>
2020-07-22 15:55:54 +0900

    ContextAPI를 사용하는 경우에는 Props를 거치지 않고 최종 목적지(컴포넌트)까지 전달이 바로 가능하다.

--- src/ContextSample.js
+++ src/ContextSample.js
@@ -1,19 +1,29 @@
-import React, { createContext, useContext, Children } from "react";
+import React, { createContext, useContext, useState } from "react";
 
-function Child({ text }) {
+const MyContext = createContext("기본값입력");
+
+function Child() {
+  const text = useContext(MyContext);
   return <div>안녕하세요 {text}</div>;
 }
 
-function Parent({ text }) {
-  return <Child text={text} />;
+function Parent() {
+  return <Child />;
 }
 
-function GrandParent({ text }) {
-  return <Parent text={text} />;
+function GrandParent() {
+  return <Parent />;
 }
 
 function ContextSample() {
-  return <GrandParent text="GOOD" />;
+  const [value, setValue] = useState(true);
+  return (
+    //  Context객체의 Provider를 이용하여 값을 지정하면 Props를 이용하지 않고도 최종 목적지인 Child까지 전달이 가능하다
+    <MyContext.Provider value={value ? "GOOD" : "BAD"}>
+      <GrandParent />
+      <button onClick={() => setValue(!value)}>CLICK ME</button>
+    </MyContext.Provider>
+  );
 }
 
 export default ContextSample;
