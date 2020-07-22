414692a309da93959a19911b4ec5a38cbf7840b2
Sannim <inegg.apps@gmail.com>
2020-07-22 15:45:37 +0900

    useReducer로 변환 완료

--- src/UseInputs.js
+++ src/UseInputs.js
@@ -1,18 +1,39 @@
-import { useState, useReducer, useCallback } from "react";
+import { useReducer, useCallback } from "react";
 
 function reducer(state, action) {
-  //CHANGE
-  //RESET
+  switch (action.type) {
+    case "CHANGE":
+      const { name, value } = action;
+      console.log(state, "응?");
+      return {
+        ...state,
+        [name]: value,
+      };
+    case "RESET":
+      return action.initialForm;
+    default:
+      throw new Error("Unhandled Exception");
+  }
 }
 //custom hook
 function useInputs(initialForm) {
-  const [form, setForm] = useState(initialForm);
+  const [state, dispatch] = useReducer(reducer, initialForm);
   const onChange = useCallback((e) => {
     const { name, value } = e.target;
-    setForm((form) => ({ ...form, [name]: value }));
+    dispatch({
+      type: "CHANGE",
+      name,
+      value,
+    });
+    console.log(name, value);
   }, []);
-  const reset = useCallback(() => setForm(initialForm), [initialForm]);
-  return [form, onChange, reset];
+  const reset = useCallback(() =>
+    dispatch({
+      type: "RESET",
+      initialForm,
+    })
+  );
+  return [state, onChange, reset];
 }
 
 export default useInputs;
