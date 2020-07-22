d8b669e634987ff8b9f95bbfa0457edcbf69d059
Sannim <inegg.apps@gmail.com>
2020-07-22 17:10:01 +0900

    Immer 사용하여 불변성 유지하기 (단순화)

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import React, { useRef, useReducer, useMemo, useCallback, createContext } from "react";
+import produce from "immer";
 import "./App.css";
 import UserList from "./UserList";
 import CreateUser from "./CreateUser";
+
+// window.produce = produce; //크롬 개발자 도구에서 사용할 수 있음
 
 function countActiveUsers(users) {
   console.log("활성 사용자를 세는 중");
@@ -34,22 +37,34 @@ const initialState = {
 function reducer(state, action) {
   switch (action.type) {
     case "CREATE_USER":
-      return {
-        inputs: initialState.inputs,
-        users: state.users.concat(action.user),
-      };
+      return produce(state, (draft) => {
+        draft.users.push(action.user);
+      });
+    // return {
+    //   inputs: initialState.inputs,
+    //   users: state.users.concat(action.user),
+    // };
     case "TOGGLE_USER":
-      return {
-        ...state,
-        users: state.users.map((user) =>
-          user.id === action.id ? { ...user, active: !user.active } : user
-        ),
-      };
+      return produce(state, (draft) => {
+        const user = draft.users.find((user) => user.id === action.id);
+        user.active = !user.active;
+      });
+    // return {
+    //   ...state,
+    //   users: state.users.map((user) =>
+    //     user.id === action.id ? { ...user, active: !user.active } : user
+    //   ),
+    // };
     case "REMOVE_USER":
-      return {
-        ...state,
-        users: state.users.filter((user) => user.id !== action.id),
-      };
+      //immer를 사용하는 경우 코드가 무조건 간결해지지는 않으므로 적절한 상황에만 사용한다.
+      return produce(state, (draft) => {
+        const index = draft.users.findIndex((user) => user.id === action.id);
+        draft.users.splice(index, 1);
+      });
+    // return {
+    //   ...state,
+    //   users: state.users.filter((user) => user.id !== action.id),
+    // };
     default:
       throw new Error("Unhandled action");
   }
