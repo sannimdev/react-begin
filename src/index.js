a2a7cc2630e4efab371bd22fbb7dbfb80009b8ea
Sannim <inegg.apps@gmail.com>
2020-07-22 14:24:02 +0900

    useReducer를 이용하여 상탯값 관리하기. useReducer(컴포넌트 밖 reducer, 초깃값);'
    reducer(상탯값, 액션){
    액션이 INCREMENT이면 => state + 1
    액션이 DECREMENT이면 => state - 1
    이외에는 예외를 발생시키거나 다른 처리를 하면 됨.
    }
    
    상탯값을 관리할 때 앞으로 useState(state+1)로 관리하는 것이 아니라 dispatch를 이용하여 type:"INCREMENT" 또는 "DECREMENT"로 전달하면
    해당하는 유형에 따라 알맞은 처리를 해주게 된다.

--- src/index.js
+++ src/index.js
@@ -2,10 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
+import Counter from "./Counter";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Counter />
   </React.StrictMode>,
   document.getElementById("root")
 );
